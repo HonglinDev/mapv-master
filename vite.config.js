2a7243f51aca7d33f6f7e24a832a8026638f1c92
李红林 <1770679549@qq.com>
2025-12-04 10:01:05 +0800

    改成vite项目

---
+++ vite.config.js
@@ -0,0 +1,36 @@
+import { defineConfig } from 'vite';
+import path from 'path';
+import { fileURLToPath } from 'url';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+export default defineConfig({
+  plugins: [
+    // 可以添加其他插件，如vue插件等，如果需要的话
+  ],
+  build: {
+    lib: {
+      entry: path.resolve(__dirname, 'index.js'),
+      name: 'mapv',
+      formats: ['umd'],
+      fileName: (format, entryName) => 'mapv.js'
+    },
+    rollupOptions: {
+      external: [
+        'maptalks',
+        'openlayers',
+        'leaflet'
+      ],
+      output: {
+        globals: {
+          openlayers: 'ol',
+          leaflet: 'L',
+          maptalks: 'maptalks'
+        }
+      }
+    },
+    outDir: 'build',
+    sourcemap: true
+  }
+});
